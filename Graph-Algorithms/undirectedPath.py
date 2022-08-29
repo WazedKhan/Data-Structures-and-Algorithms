0f3e120cabd3b58fe12ec9bb0b8926556f2d3ac6
Wazed Khan <51821594+WazedKhan@users.noreply.github.com>
2022-08-29 20:23:15 +0600

    iteratively

---
+++ Graph-Algorithms/undirectedPath.py
@@ -0,0 +1,40 @@
+edges = [
+  ('i', 'j'),
+  ('k', 'i'),
+  ('m', 'k'),
+  ('k', 'l'),
+  ('o', 'n')
+]
+
+def undirectedPath(edges, source, destination):
+    graph = build_graph(edges)
+    stack = [source]
+    visited = set()
+    while len(stack) > 0:
+        current = stack.pop()
+        if current == destination:
+            return True
+        for neighbor in graph[current]:
+            if neighbor not in visited:
+                stack.append(neighbor)
+                visited.add(neighbor)
+
+    return False
+
+
+
+def build_graph(edges):
+    graph = {}
+    for edge in edges:
+        a, b = edge
+        if a not in graph:
+            graph[a] = []
+        if b not in graph:
+            graph[b] = []
+        graph[a].append(b)
+        graph[b].append(a)
+
+    return graph
+
+undirectedPath(edges, 'j', 'm')
+

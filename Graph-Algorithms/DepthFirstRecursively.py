0b67ef69aeee515c54973ce956c41d8d908106c7
Wazed Khan <51821594+WazedKhan@users.noreply.github.com>
2022-08-29 18:27:41 +0600

    Depth First Recursively

---
+++ Graph-Algorithms/DepthFirstRecursively.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+def depthFirstPrint(graph, source):
+    print(source)
+    for neighbor in graph[source]:
+        depthFirstPrint(graph, neighbor)
+
+graph = {
+    'a':['b', 'c'],
+    'b':['d'],
+    'c':['e'],
+    'd':['f'],
+    'e':[],
+    'f':[]
+}
+
+"""
+(a)----(c)
+ |      |
+(b)    (e)
+ |
+(d)----(f)
+ans: a->b->d->f->c-e
+"""
+
+depthFirstPrint(graph, 'a')

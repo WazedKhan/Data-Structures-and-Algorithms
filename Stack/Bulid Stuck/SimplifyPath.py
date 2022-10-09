f08c55428b1ad70bed3e8c8a3205b4625b9bc849
Wazed Khan <51821594+WazedKhan@users.noreply.github.com>
2022-10-09 22:15:58 +0600

    stack problems set-1

---
+++ Stack/Bulid Stuck/SimplifyPath.py
@@ -0,0 +1,13 @@
+def simplifyPath( path):
+    res = []
+    for i in path.split("/"):
+        if i != '' and i != '.' and i != '..':
+            res.append(i)
+        if i == '..' and res:
+            res.pop()
+
+    return '/'+'/'.join(res)
+
+path = "/home/../../temp//./"
+
+print(simplifyPath(path))
